0dc734b5167ade0ec5528dc711cda510477df7cf
Dennys Pucha <dennys.pucha@unl.edu.ec>
2024-02-14 17:59:40 -0500

    first commit

---
+++ app/controls/ComentarioControl.js
@@ -0,0 +1,230 @@
+"use strict";
+var models = require("../models");
+var persona = models.persona;
+var rol = models.rol;
+var cuenta = models.cuenta;
+var comentario = models.comentario;
+var noticia = models.noticia;
+class ComentarioControl {
+    async obtener(req, res) {
+        const external = req.params.external;
+        var lista = await comentario.findOne({
+            where: { external_id: external },
+            include: [
+                {
+                    model: models.noticia,
+                    as: "noticia",
+                    attributes: ["titulo", "cuerpo", "fecha", "estado"],
+                },
+            ],
+            attributes: [
+                "texto",
+                "fecha",
+                "estado",
+                "usuario",
+                "latitud",
+                "longitud",
+                "external_id",
+                "id_noticia"
+            ],
+        });
+        if (lista == undefined || lista == null) {
+            res.status(404);
+            res.json({ msg: "No encontrado", code: 404, datos: {} });
+        } else {
+            res.status(200);
+            res.json({ msg: "OK", code: 200, datos: lista });
+        }
+    }
+
+    async listar(req, res) {
+        var lista = await comentario.findAll({
+            include: [
+                {
+                    model: models.noticia,
+                    as: "noticia",
+                    attributes: ["titulo", "cuerpo", "fecha", "estado"],
+                },
+            ],
+            attributes: [
+                "texto",
+                "fecha",
+                "estado",
+                "usuario",
+                "latitud",
+                "longitud",
+                "external_id",
+                "id_noticia",
+            ],
+        });
+        res.status(200);
+        res.json({ msg: "OK", code: 200, datos: lista });
+    }
+
+    async guardar(req, res) {
+        if (
+            req.body.hasOwnProperty("texto") &&
+            req.body.hasOwnProperty("fecha") &&
+            req.body.hasOwnProperty("usuario") &&
+           // req.body.hasOwnProperty("latitud") &&
+            //req.body.hasOwnProperty("longitud") &&
+            req.body.hasOwnProperty("noticia")
+        ) {
+            var uuid = require("uuid");
+            var noticiaA = await models.noticia.findOne({
+                where: { external_id: req.body.noticia },
+            });
+
+            if (noticiaA == undefined) {
+                res.status(400);
+                res.json({ msg: "ERROR", tag: "Noticia no existente", code: 400 });
+            }
+            var personaA = await models.persona.findOne({
+                where: { external_id: req.body.usuario },
+            });
+
+            if (noticiaA == undefined) {
+                res.status(400);
+                res.json({ msg: "ERROR", tag: "Noticia no existente", code: 400 });
+            } else if (personaA == undefined) {
+                res.status(400);
+                res.json({ msg: "ERROR", tag: "Persona no existente", code: 400 });
+            } else {
+                var rolA = await models.rol.findOne({ where: { id: personaA.id_rol } });
+                if (rolA.nombre !== "user") {
+                    res.status(400);
+                    res.json({
+                        msg: "ERROR",
+                        tag: "La persona no tiene el rol 'user'",
+                        code: 400,
+                    });
+                } else {
+                    var data = {
+                        texto: req.body.texto,
+                        fecha: req.body.fecha,
+                        usuario: personaA.nombres + " " + personaA.apellidos,
+                        latitud: req.body.latitud,
+                        longitud: req.body.longitud,
+                        external_id: uuid.v4(),
+                        id_noticia: noticiaA.id,
+                    };
+                    let transaction = await models.sequelize.transaction();
+                    try {
+                        var result = await comentario.create(data, { transaction });
+                        await transaction.commit();
+                        if (result === null) {
+                            res.status(401);
+                            res.json({ msg: "ERROR", tag: "No se puede crear", code: 401 });
+                        } else {
+                            res.status(200);
+                            res.json({ msg: "OK", code: 200 });
+                        }
+                    } catch (error) {
+                        if (transaction) await transaction.rollback();
+                        res.status(203);
+                        res.json({ msg: "ERROR", code: 203, error_msg: error });
+                    }
+                }
+            }
+        } else {
+            res.status(400);
+            res.json({ msg: "ERROR", tag: "Datos incorrectos", code: 400 });
+        }
+    }
+
+    async modificar(req, res) {
+        if (
+            req.body.hasOwnProperty("texto") &&
+            req.body.hasOwnProperty("fecha") &&
+            req.body.hasOwnProperty("estado") &&
+            req.body.hasOwnProperty("usuario") &&
+            //req.body.hasOwnProperty("latitud") &&
+            //req.body.hasOwnProperty("longitud") &&
+            req.body.hasOwnProperty("noticia")
+        ) {
+            const external = req.params.external;
+            try {
+                const comentarioA = await comentario.findOne({
+                    where: { external_id: external },
+                });
+                if (!comentarioA) {
+                    res.status(404);
+                    return res.json({
+                        msg: "ERROR",
+                        tag: "Comentario no encontrado",
+                        code: 404,
+                    });
+                }
+                var noticiaA = await noticia.findOne({
+                    where: { external_id: req.body.noticia },
+                });
+
+                var personaA = await models.persona.findOne({
+                    where: { external_id: req.body.usuario },
+                });
+
+                if (noticiaA == undefined) {
+                    res.status(400);
+                    res.json({ msg: "ERROR", tag: "Noticia no existente", code: 400 });
+                } else {
+                    var rolA = await models.rol.findOne({
+                        where: { id: personaA.id_rol },
+                    });
+                    if (rolA.nombre !== "user") {
+                        res.status(400);
+                        res.json({
+                            msg: "ERROR",
+                            tag: "La persona no tiene el rol 'user'",
+                            code: 400,
+                        });
+                    } else {
+                        const data = {
+                            texto: req.body.texto,
+                            fecha: req.body.fecha,
+                            estado: req.body.estado,
+                            usuario: personaA.nombres + " " + personaA.apellidos,
+                            latitud: req.body.latitud,
+                            longitud: req.body.longitud,
+                            id_noticia: noticiaA.id,
+                        };
+                        const transaction = await models.sequelize.transaction();
+                        try {
+                            await comentarioA.update(data, { transaction });
+                            await transaction.commit();
+                            res.status(200);
+                            res.json({ msg: "OK", code: 200 });
+                        } catch (error) {
+                            if (transaction) await transaction.rollback();
+                            res.status(203);
+                            res.json({ msg: "ERROR", code: 203, error_msg: error });
+                        }
+                    }
+                }
+            } catch (error) {
+                res.status(500);
+                res.json({ msg: "ERROR", code: 500, error_msg: error });
+            }
+        } else {
+            res.status(400);
+            res.json({ msg: "ERROR", tag: "Datos incorrectos", code: 400 });
+        }
+    }
+
+    async obtenerComentarios(req, res) {
+        const external = req.params.external;
+        const noticiaA = await noticia.findOne({where: { external_id: external },});
+
+        var lista = await comentario.findAll({
+            where: { id_noticia: noticiaA.id },
+            attributes: ['texto', 'fecha', 'estado', 'usuario', 'latitud', 'longitud', 'external_id']
+        });
+        if (lista == undefined || lista == null) {
+            res.status(404);
+            res.json({ msg: "No encontrado", code: 404, datos: {} });
+        } else {
+            res.status(200);
+            res.json({ msg: "OK", code: 200, datos: lista });
+        }
+    }
+}
+module.exports = ComentarioControl;
---
+++ routes/users.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var router = express.Router();
+let jwt =require('jsonwebtoken');
+const personaC=require('../app/controls/PersonaControl')
+let personaControl=new personaC();
+
+const rolC=require('../app/controls/RolControl')
+let rolControl=new rolC();
+
+const cuentaC=require('../app/controls/CuentaControl')
+let cuentaControl=new cuentaC();
+
+const noticiaC=require('../app/controls/NoticiaControl')
+let noticiaControl=new noticiaC();
+
+const comentarioC=require('../app/controls/ComentarioControl')
+let comentarioControl=new comentarioC();
+
+const auth=function middleware(req,res,next){
+    const token =req.headers['news-token'];
+    console.log(token);
+    if(token===undefined){
+      res.status(401);
+      res.json({ msg: "Falta Token", code: 401 });
+    }else{
+      require('dotenv').config();
+      const key=process.env.KEY_SEC;
+      jwt.verify(token,key,async(err,decoded)=>{
+        if(err){
+          res.status(401);
+          res.json({ msg: "ERROR",tag:'token no valido o expirado', code: 401 });
+        }else{
+          console.log(decoded.external);
+          const models=require('../app/models');
+          const cuenta=models.cuenta;
+          const aux=await cuenta.findOne({
+            where: {external_id:decoded.external}
+         });
+         if(aux===null){
+          res.status(401);
+          res.json({ msg: "ERROR",tag:'token no valido', code: 401 });
+         }else{
+          next();
+        }
+        }
+
+      });
+    }
+    //console.log(req.url);
+    //next();
+}
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+
+router.get('/admin/personas',personaControl.listar);
+router.post('/admin/persona/save',personaControl.guardar);
+router.get('/admin/persona/get/:external',personaControl.obtener);
+router.post('/admin/persona/modificar/:external',personaControl.modificar);
+router.post('/admin/persona/saveUser',personaControl.guardarUsuario);
+router.post('/admin/persona/modificarUser/:external',personaControl.modificarUsuario);
+
+router.get('/admin/roles',rolControl.listar);
+router.post('/admin/rol/save',rolControl.guardar);
+
+router.post('/admin/inicio_sesion',cuentaControl.inicio_sesion);
+router.get('/admin/cuentas',cuentaControl.listar);
+
+
+router.get('/admin/noticias',noticiaControl.listar);
+router.get('/admin/noticia/get/:external',noticiaControl.obtener);
+router.post('/admin/noticia/save',noticiaControl.guardar);
+router.post('/admin/noticia/modify/:external',noticiaControl.modificar);
+router.post('/admin/noticia/file/save/:external',noticiaControl.guardarFoto);
+
+
+router.get('/admin/comentarios',comentarioControl.listar);
+router.get('/admin/comentario/get/:external',comentarioControl.obtener);
+router.post('/admin/comentario/save',comentarioControl.guardar);
+router.post('/admin/comentario/modify/:external',comentarioControl.modificar);
+router.get('/admin/noticia/get/comentarios/:external',comentarioControl.obtenerComentarios);
+module.exports = router;
